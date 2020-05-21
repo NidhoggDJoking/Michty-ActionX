89dad9e3d97ef966f9f79d02f68f77326ff595db
joking <250348426@qq.com>
2020-05-21 21:35:25 +0800

    配置

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 var hx = require("hbuilderx");
 //该方法将在插件激活的时候调用
 function activate(context) {
+	hx.window.setStatusBarMessage('　愛にできることはまだあるかい　,　僕にできることはまだあるかい　!', 0, 'info');
 	let disposable = hx.commands.registerCommand('extension.helloWorld', () => {
 		hx.window.showInformationMessage('<h3>你好，这是Joking的专属插件扩展!</h3>');
 	});
